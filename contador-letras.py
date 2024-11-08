19befe94451b1aa171e24915dccd4c1bd3e75ad0
Miguel <miguemartinelli@gmail.com>
2024-11-08 13:33:27 -0300

    Realizamos algoritmo

---
+++ contador-letras.py
@@ -0,0 +1,13 @@
+# // 1. Solicitar entrada al usuario
+
+palabra_ingresada = input("Por favor, ingrese la palabra: ")
+
+# // 2. Contar la cantidad de letras 
+
+cant_letras = len(palabra_ingresada)
+
+#// 3. Mostrar al usuario el resultado
+
+print("La palabra ingresada tiene ", cant_letras, "letras.")
+
+
